93394f0379ed2b694831f66d69b7bdcd62e69886
minju9927 <pink5214@naver.com>
2025-03-28 11:26:27 +0900

    Add files via upload

---
+++ src/components/Section1.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import './Section1.scss'
+import Aos from 'aos'
+import 'aos/'
+
+const Section1 = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1000
+        })
+    }, [])
+
+
+    
+        return (
+            
+                <section className='s1'>
+                    <div className="rel">
+                        <h2>section2 title</h2>
+                        <ul className="content">
+                        <li data-aos="fade-up" data-aos-delay="">1</li>
+                        <li data-aos="fade-up" data-aos-delay="100">2</li>
+                            <li data-aos="fade-up" data-aos-delay="200" >3</li>
+                            <li data-aos="fade-up" data-aos-delay="300">4</li>
+                            <li data-aos="fade-up" data-aos-delay="400">5</li>
+                        </ul>
+                    </div>
+                </section>
+            
+        )
+    }
+
+    export default Section1
